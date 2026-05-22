c8bc02faa67194b4df19ab2924e0a85a68589643
kocakcan <can_kocak1@epam.com>
2026-05-22 10:30:38 +0300

    completed alignment

--- chapter_2/objs_funcs_and_types.c
+++ chapter_2/objs_funcs_and_types.c
@@ -258,8 +258,63 @@
 * A constant value refers to literal constants (for example, 1, 'a', 0xFF), enum membe
 * rs, and the results of operators such as alignof or sizeof; not const-qualified obje
 * cts.
+*
+* Alignment
+*
+* Object types have alignment requirements that place restrictions on the addresses at
+* which objects of that type may be allocated. An alignment represents the number of b
+* ytes between successive addresses at which a given object can be allocated. CPUs may
+* have different behaviour when accessing aligned data (for example, the data address 
+* is a multiple of the data size) versus unaligned data.
+* 	Some machine instructions can perform multiple accesses on non-word boundaries
+* , but there may be a performance penalty. A word is a natural, fixed-size unit of a 
+* data handled by the instruction set or the hardware of the processor. Some platforms
+* cannot access unaligned memory. Alignment requirements may depend on the CPU word si
+* ze (typically, 16, 32, or 64 bits).
+* 	Generally, C programmers need not concern themselves with alignment requiremen
+* ts, because the compiler chooses suitable alignments for its various types. Dynamica
+* lly allocated memory from malloc is required to be sufficiently aligned for all stan
+* dard types, including arrays and structures. However, on rare occasions, you might n
+* eed to override the compiler's default choices; for example, to align data on the bo
+* undaries of the memory cache lines that must start at power-of-two address boundarie
+* s, or to meet other system-specific requirements. Traditionally, these requirements 
+* were met by linker commands, or by overallocating memory with malloc followed by rou
+* nding the user address upward, or similar operations involving other nonstandard fac
+* ilties.
+* 	C11 introduced a simple, forward-compatible mechanism for specifying alignment
+* s. Alignments are represented as values of type size_t. Every valid alignment value 
+* is a nonnegative integral power of two. An object type imposes a default alignment 
+* requirement on every object of that type: a stricter alignment (a larger power of tw
+* o) can be requested using the alignment specifier (_Alignas). You can include an ali
+* gnment specifier in the declaration specifiers of a declaration. Listing 2-7 uses th
+* e alignment specifier to ensure that good_buff is properly aligned (bad_buff) may ha
+* ve incorrect alignment for member-access expressions).
+*
+* 	struct S {
+*		int i;
+*		double d;
+*		char c;
+*	};
+*
+*	int main(void) {
+*		unsigned char bad_buff[sizeof(struct S)];
+*		_Alignas(struct S) unsigned char good_buff[sizeof(struct S)];
+*
+*		struct S *bad_s_ptr = (struct S *)bad_buff; // wrong pointer alignment
+*		struct S *good_s_ptr = (struct S *)good_buff; // correct pointer alig.
+*	}
+*	Listing 2-7: Use of the _Alignas keyword
+* 	Alignments are ordered from weaker to stronger (also called stricter) alignmen
+* ts. Stricter alignments have larger alignment values. And address that satisfies an 
+* alignment requirement also satisfies any valid, weaker alignment requirement.
  */
 #include <stdio.h>
+
+struct S {
+	int i;
+	double d;
+	char c;
+};
 
 void swap(int *pa, int *pb) {			// pa -> a: 21	pb -> b: 17
 	int t = *pa;				// t: 21
@@ -273,5 +328,10 @@ int main(void) {
 	int b = 17;				// b: 17
 	swap(&a, &b);
 	printf("a = %d, b = %d\n", a, b);	// a: 17	b: 21
+	unsigned char bad_buff[sizeof(struct S)];
+	_Alignas(struct S) unsigned char good_buff[sizeof(struct S)];
+
+	struct S *bad_s_ptr = (struct S *)bad_buff;
+	struct S *good_s_ptr = (struct S *)good_buff;
 	return 0;
 }
